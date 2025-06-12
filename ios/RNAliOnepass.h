bd8299bf05e4e53862d2ad3cd5ad2a8f79b9ba52
fuxi <806941929@qq.com>
2025-06-12 21:06:00 +0800

    Revert "Revert "fix""
    This reverts commit 330c40678ec4cf763f59ae894db62b0d7f9d69b4.

--- ios/RNAliOnepass.h
+++ ios/RNAliOnepass.h
@@ -1,21 +1,24 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-// 检测运行环境并配置相应的SDK导入
+// 检测是否在模拟器环境或强制模拟模式
 #if TARGET_OS_SIMULATOR || defined(RN_ALI_ONEPASS_SIMULATOR)
-// 模拟器环境：使用模拟实现
+// 模拟器环境下不导入阿里SDK
+// 在这里声明必要的类型，避免编译错误
 @class TXCommonHandler;
 @class TXCustomModel;
-
-#elif defined(RN_ALI_ONEPASS_DEVICE) && __has_include(<ATAuthSDK/ATAuthSDK.h>)
-// 真机环境且SDK可用：导入阿里SDK
-#import <ATAuthSDK/ATAuthSDK.h>
-#import <ATAuthSDK/PNSReturnCode.h>
 
 #else
-// 回退模式：SDK不可用时使用模拟实现
+// 真机环境下导入阿里SDK
+#if __has_include(<ATAuthSDK/ATAuthSDK.h>)
+#import <ATAuthSDK/ATAuthSDK.h>
+#import <ATAuthSDK/PNSReturnCode.h>
+#else
+// 如果头文件不存在，使用模拟模式
+#define RN_ALI_ONEPASS_FALLBACK_SIMULATOR 1
 @class TXCommonHandler;
 @class TXCustomModel;
+#endif
 #endif
 
 @interface RNAliOnepass : RCTEventEmitter <RCTBridgeModule>
